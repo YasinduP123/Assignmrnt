823405750d4dd4b683d149d1148ad86510bb00da
Yasidu Pathiraja <yasindupathiraja10@gmail.com>
2024-06-28 23:17:21 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+    console.log("Hello World")
+
+    let name = "saman";
+    let age = 12;
+    let isActive = false;
+    let address = undefined;
+
+    console.log( name);
+    console.log( age);
+    console.log( isActive);
+    console.log( address);
+
+    
